d73e84689a329d7aca073a619d6a76528b3a7345
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-29 04:42:02 +0000

    Lists states and cities by name

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+starts a flask web app listening on port 5000
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__, template_folder="templates")
+
+
+@app.teardown_appcontext
+def app_teardown(exception=None):
+    """ Removes SQLAlchemy Session after each request """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_state():
+    """ rendering template data from state """
+    return render_template('8-cities_by_states.html',
+                           states=storage.all("State"),
+                           cities=storage.all("City"))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
