e57e64e0a4ef63f08ba54f8e70b39e649b3aa91f
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-29 04:20:45 +0000

    Add task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__, template_folder="templates")
+
+
+@app.teardown_appcontext
+def app_teardown(exception=None):
+    """ Removes SQLAlchemy Session after each request """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_state():
+    """ rendering template data from state """
+    return render_template('7-states_list.html',
+                           states=storage.all("State"))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
